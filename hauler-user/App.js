7b0a48c92da2905f8ed4e4d7a6ec1435e70b237c
Brayden <dudgjs1318@gmail.com>
2023-04-18 11:57:49 -0700

    install hauler-user necessary packages

--- hauler-user/App.js
+++ hauler-user/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>We worked it out installing all legacy packages to up to date!</Text>
       <StatusBar style="auto" />
     </View>
   );
