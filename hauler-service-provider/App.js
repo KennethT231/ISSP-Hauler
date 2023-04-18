0fd89205a7ef77c770b022dc3b90b912b02ae361
Brayden <dudgjs1318@gmail.com>
2023-04-18 12:09:23 -0700

    install all neccessary packages for hauler-service-provider section

--- hauler-service-provider/App.js
+++ hauler-service-provider/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js from service provider!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
--- hauler-user/App.js
+++ hauler-user/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>We worked it out installing all legacy packages to up to date!</Text>
+      <Text>Open up App.js from user!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
