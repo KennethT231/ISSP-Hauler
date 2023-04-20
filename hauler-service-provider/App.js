080cba0e499a9d780d4209cb3f52d755c84cb72f
lisa1204pm <dlee560@my.bcit.ca>
2023-04-19 17:08:51 -0700

    bringing old code

--- hauler-service-provider/App.js
+++ hauler-service-provider/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js from service provider!!!</Text>
+      <Text>Open up App.js from Provider!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ hauler-service-provider/src/components/userInfo/UserInfo.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { TextInput, View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+export default function UserInfo({ firstName, lastName, province, city, streetAddress,
+    unitNumber, setCity, setStreetAddress, setUnitNumber, profilePicUrl, dateOfBirth, setDob, contactNumber, setContactNumber, setProvince, setFirstName, setLastName, setProfilePicUrl, setError }) {
+    return (
+        <View>
+             <Text style={styles.text1}> profilePicUrl : </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(profilePic) => { setError(""); setProfilePicUrl(profilePic) }}
+                value={profilePicUrl}
+            />
+             <Text style={styles.text1}> First Name : </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(fname) => { setError(""); setFirstName(fname) }}
+                value={firstName}
+            />
+             <Text style={styles.text1}> Last Name : </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(lname) => { setError(""); setLastName(lname) }}
+                value={lastName}
+            />
+             <Text style={styles.text1}> Date of Birth : </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(date) => { setError(""); setDob(date) }}
+                value={dateOfBirth}
+            />
+             <Text style={styles.text1}> Province : </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(province) => { setError(""); setProvince(province) }}
+                value={province}
+            />
+             <Text style={styles.text1}> City : </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(city) => { setError(""); setCity(city) }}
+                value={city}
+            />
+             <Text style={styles.text1}> StreetAddress : </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(streetAddress) => { setError(""); setStreetAddress(streetAddress) }}
+                value={streetAddress}
+            />
+             <Text style={styles.text1}> Unit Number : </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(unitNumber) => { setError(""); setUnitNumber(unitNumber) }}
+                value={unitNumber}
+            />
+             <Text style={styles.text1}> Contact Number : </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(number) => { setError(""); setContactNumber(number) }}
+                value={contactNumber}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    input: {
+        borderBottomColor: '#BFBFBF',
+        borderBottomWidth: 1,
+        height: 40,
+        overflow: 'hidden',
+        backgroundColor: 'white',
+        marginHorizontal: '5%',
+        marginBottom: 20
+    },
+    text1: {
+        color: '#BFBFBF',
+        marginLeft: '5%'
+    },
+})
+
