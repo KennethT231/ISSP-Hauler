eb68a8eb9ef492d0a8f7f4a43ca814f1a5841209
cashcrof <ceilinatrix@gmail.com>
2023-04-28 11:35:24 -0700

    removed changed files from user

--- hauler-user/src/components/SelectWeight/SelectWeight.js
+++ hauler-user/src/components/SelectWeight/SelectWeight.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import RNPickerSelect from 'react-native-picker-select-updated-updated';
+import RNPickerSelect from 'react-native-picker-select-updated';
 
 export default function SelectWeight({ selectedweight, setSelectedWeight }) {
 
