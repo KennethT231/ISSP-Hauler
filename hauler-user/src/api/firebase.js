dbcb3a22f4b377df3625184a27a91e702c586e6f
Mario Lopez <jlopezvillarreal@my.bcit.ca>
2023-05-05 11:20:57 -0700

    env

--- hauler-user/src/api/firebase.js
+++ hauler-user/src/api/firebase.js
@@ -1,14 +1,21 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/storage";
+import { REACT_APP_FIREBASE_API_KEY, 
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID
+} from '@env';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyClEdxZbnsmTQJJbcRS5qTinwf0njmAC94",
-    authDomain: "hauler-44066.firebaseapp.com",
-    projectId: "hauler-44066",
-    storageBucket: "hauler-44066.appspot.com",
-    messagingSenderId: "236642426134",
-    appId: "1:236642426134:web:b451c56241619c3af00437"
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID
 };
 
 
